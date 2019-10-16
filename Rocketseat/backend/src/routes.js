e7bda7bb60aeedfe75a33beb1e78721aeb582e10
alinevitoriasi <alinevitoriabsi@gmail.com>
2019-10-16 10:51:55 -0300

    src

--- Rocketseat/backend/src/routes.js
+++ Rocketseat/backend/src/routes.js
@@ -1,24 +1,17 @@
-// const express = require('express')
+//req.params = Acessar route params (para edição, delete)
+//req.query é para acessar os query params (para filtros)
+//req.body = acessa corpo da requisição
+// GET, POST, PUT, DELETE
+
+const express = require('express')
 // const SessionController = require('./controllers/SessionController')
 // const routes= express.Router()
-
-
 
 // routes.post('/sessions',SessionController.store)
 // routes.post('/spots',SpotController.store)
 
-// module.exports = routes
 
-
-const app = require('express')
-
-//req.params = Acessar route params (para edição, delete)
-//req.query é para acessar os query params (para filtros)
-//req.body = acessa corpo da requisição
-
-
-// GET, POST, PUT, DELETE
-var routes = app.Router()
+var routes = express.Router()
 
 routes.post('/users', (req, res) => {
     return res.json(req.body)
